dae33ca878e58ebce234c5d4c5e1113110708ad1
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-12 14:55:56 +0200

    add modal functional

--- js/script.js
+++ js/script.js
@@ -88,4 +88,40 @@ window.addEventListener('DOMContentLoaded', () =>{
 
     setClock('.timer', deadLine)
 
+    //Подключаю Модальное окно
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn=> {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden'; //Убираю прокрутку за модальным окном
+        });
+    });
+
+    //Убираю повторение в коде(не повторяйся)
+    function closeModal(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    //Событие на кнопку закрыть(Х)
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    // При клике на обложку модального окна - закроется окно
+    modal.addEventListener('click', (e)=>{
+        if(e.target === modal){
+            closeModal();
+        }
+    })
+
+    //Закрывание модалки по нажатию клавиши Esc
+    document.addEventListener('keydown', e => {
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal();
+        }
+    })
 })
