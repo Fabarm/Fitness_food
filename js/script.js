0f7e0ed1eb6202bc444613d31f117eef1dc96f6f
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-13 20:24:14 +0200

    install json-serverand added db.json

--- js/script.js
+++ js/script.js
@@ -326,4 +326,8 @@ window.addEventListener('DOMContentLoaded', () =>{
         }, 3000);
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data=>data.json())
+        .then(res => console.log(res))
+
 })
