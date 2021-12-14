1635254f16fe3059fcc80d9ea43e2f018f0bd10d
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-14 22:56:08 +0200

    applied get and post requests plus database.json

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () =>{
             item.classList.add('hide');
             item.classList.remove('show', 'fade');
         });
-        tabs.forEach(item=>{
+        tabs.forEach( item => {
             item.classList.remove('tabheader__item_active');
         });
     }
@@ -184,35 +184,52 @@ window.addEventListener('DOMContentLoaded', () =>{
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        "Меню \"Фитнес\"",
-        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
-        9,
-        ".menu .container",
-        "menu__item"
-    ).render();
+    const getResource = async (url, data) => {
+        const res = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        14,
-        ".menu .container",
-        "menu__item"
-    ).render();
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        21,
-        ".menu .container",
-        "menu__item"
-    ).render();
+        return await res.json();
+    };
+
+    getResource('http://localhost:3000/menu')
+    .then(data=>{
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+    })
+
+    // new MenuCard(
+    //     "img/tabs/vegy.jpg",
+    //     "vegy",
+    //     "Меню \"Фитнес\"",
+    //     "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+    //     9,
+    //     ".menu .container",
+    //     "menu__item"
+    // ).render();
+
+    // new MenuCard(
+    //     "img/tabs/post.jpg",
+    //     "post",
+    //     'Меню "Постное"',
+    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    //     14,
+    //     ".menu .container",
+    //     "menu__item"
+    // ).render();
+
+    // new MenuCard(
+    //     "img/tabs/elite.jpg",
+    //     "elite",
+    //     'Меню “Премиум”',
+    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    //     21,
+    //     ".menu .container",
+    //     "menu__item"
+    // ).render();
 
     //Отправка формы
 
@@ -222,13 +239,24 @@ window.addEventListener('DOMContentLoaded', () =>{
         loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
         failure: 'Что-то пошло не так...'
-    }
+    };
 
     forms.forEach(item => {
-        postData(item);
-    })
+        bindPostData(item);
+    });
 
-    function postData(form){
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'aplication/json'
+            },
+            body: data
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form){
         form.addEventListener('submit', (e)=>{
             e.preventDefault();
 
@@ -275,18 +303,11 @@ window.addEventListener('DOMContentLoaded', () =>{
 
             const formData = new FormData(form);
 
-            const object={}
-                formData.forEach(function (value, key){
-                    object[key] = value;
-                });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'aplication/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => {
+            
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
                 statusMessage.remove();
@@ -295,11 +316,11 @@ window.addEventListener('DOMContentLoaded', () =>{
             }).finally(()=>{
                 form.reset();
             });
-        });
+        })
     }
 
     // Добавляю спинер и уведомления на модальное окно
-    function showThanksModal(message) {
+    function showThanksModal(message){
         const prevModalDialog = document.querySelector('.modal__dialog');
 
         prevModalDialog.classList.add('hide');
@@ -324,5 +345,6 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
 
     
+
 
 });
