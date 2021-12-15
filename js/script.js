9ce256e390c23e46f1eba6408bb32a215b743144
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-15 23:52:24 +0200

    added modules from webpack and fix code

--- js/script.js
+++ js/script.js
@@ -109,12 +109,7 @@ window.addEventListener('DOMContentLoaded', () =>{
         btn.addEventListener('click', openModal);
     });
 
-    //Событие на кнопку закрыть(Х)
-    // modalCloseBtn.addEventListener('click', closeModal);
-    // убрали так как добавляем окно благодарности, а в динамически
-    // созданом окне не будет работать. + Добавляем ниже в проверку
-    // ( || e.target.getAttribute('data-close') == ''
-
+    
 
     // При клике на обложку модального окна - закроется окно
     modal.addEventListener('click', (e)=>{
@@ -201,35 +196,6 @@ window.addEventListener('DOMContentLoaded', () =>{
         });
     })
 
-    // new MenuCard(
-    //     "img/tabs/vegy.jpg",
-    //     "vegy",
-    //     "Меню \"Фитнес\"",
-    //     "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
-    //     9,
-    //     ".menu .container",
-    //     "menu__item"
-    // ).render();
-
-    // new MenuCard(
-    //     "img/tabs/post.jpg",
-    //     "post",
-    //     'Меню "Постное"',
-    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    //     14,
-    //     ".menu .container",
-    //     "menu__item"
-    // ).render();
-
-    // new MenuCard(
-    //     "img/tabs/elite.jpg",
-    //     "elite",
-    //     'Меню “Премиум”',
-    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    //     21,
-    //     ".menu .container",
-    //     "menu__item"
-    // ).render();
 
     //Отправка формы
 
@@ -266,40 +232,10 @@ window.addEventListener('DOMContentLoaded', () =>{
                 display: block;
                 margin: 0 auto;
             `;
-            // form.append(statusMessage); Чтоб не рвалась верстка делаем как ниже
+            
             form.insertAdjacentElement('afterend', statusMessage);
 
-        //XMLHttpRequest переделал на Fetch()
-            // const request = new XMLHttpRequest();
-            // request.open("POST", "server.php");
-
-
-            // В ФОРМАТЕ FORM-DATA
-            // request.setRequestHeader('Content-type', 'multipart/form-data')
-            // const formData = new FormData(form);
-            // request.send(formData);
-
-            //В ФОРМАТЕ JSON
-            // request.setRequestHeader('Content-type', 'application/json') Переделал на Fetch()
-            // const formData = new FormData(form);
-
-            // const object={}
-            //     formData.forEach(function (value, key){
-            //         object[key] = value;
-            //     })
-            //
-            // const json = JSON.stringify(object)
-            // request.send(json);
-            // request.addEventListener('load', ()=> {
-            //     if(request.status === 200){
-            //         console.log(request.response);
-            //         showThanksModal(message.success);
-            //         form.reset();
-            //         statusMessage.remove();
-            //     } else {
-            //         showThanksModal(message.failure);
-            //     }
-            // })
+        
 
             const formData = new FormData(form);
 
